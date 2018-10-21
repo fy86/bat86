6ca5eabc4c28a5448797f944c5a63e764b27f4e0
fy86 <fy86@protonmail.com>
2018-10-21 11:57:44 +0800

    ipv6 setup

---
+++ win/p6016.bat
@@ -0,0 +1,8 @@
+ipconfig /flushdns
+nslookup -query=aaaa 6016.duckdns.org 208.67.220.220
+nslookup -query=aaaa 6016.duckdns.org 208.67.220.220
+nslookup -query=aaaa 6016.duckdns.org 208.67.220.220
+nslookup -query=aaaa 6016.duckdns.org
+nslookup -query=aaaa 6016.duckdns.org
+nslookup -query=aaaa 6016.duckdns.org
+ping -6 6016.duckdns.org
